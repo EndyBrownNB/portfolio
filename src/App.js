64643fa19d8a12af5239e078c2b9d1196efb163f
EndyBrownNB <endybrown1987@gmail.com>
2023-05-14 08:50:09 +0300

    first cmt

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState,useRef, useMemo, useEffect } from 'react';
+//import ClassCounter from './components/ClassCounter';
+//import MySelect from './components/UI/select/MySelect';
+//import PostItem from './components/PostItem';
+//import MyInput from './components/UI/input/MyInput';
+import './styles/App.css';
+import PostList from './components/PostList';
+import MyButton from './components/UI/button/MyButton';
+import PostForm from './components/PostForm';
+import {usePosts} from './hooks/usePosts';
+import PostFilter from './components/PostFilter';
+import MyModal from './components/UI/MyModal/MyModal';
+import axios from 'axios';
+import PostService from './API/PostService';
+import Loader from './components/UI/Loader/Loader';
+import { useFetching } from './hooks/useFetching';
+
 
 function App() {
+
+    const [posts, setPosts] = useState([])
+    const [filter,setFilter] = useState({sort:'', query:''})
+    const [modal,setModal] = useState(false)
+    const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
+    const[fetchPosts, isPostLoading, postError] = useFetching( async ()=>{
+      const posts = await PostService.getAll();
+      setPosts(posts);
+    })
+
+    useEffect(()=>{
+      fetchPosts()
+    }, [])
+    
+    const createPost = (newPost)=>{
+      setPosts([...posts, newPost])
+      setModal(false)
+    }
+
+    const removePost = (post)=>{
+        setPosts(posts.filter(p => p.id !== post.id))
+    }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <MyButton style={{marginTop: '30px'}} onClick={()=> setModal(true)}>
+      Create Post
+    </MyButton>
+      <MyModal visible={modal} setVisible={setModal}>
+        <PostForm create={createPost}/>
+      </MyModal>
+      <hr style={{margin:'15px 0'}}/>
+      <PostFilter
+        filter={filter}
+        setFilter={setFilter}
+      />
+      {postError &&
+        <h1>Error ${postError}</h1>
+      }
+      {isPostLoading
+        ? <div style={{display:'flex', justifyContent:'center', marginTop:'50px'}}><Loader/></div>
+        : <PostList remove={removePost} posts={sortedAndSearchedPosts} title='List of JavaScript'/>
+      }
+
     </div>
   );
 }
---
+++ src/components/PostItem.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+import Mybutton from "./UI/button/MyButton";
+
+const PostItem = (props) =>{
+
+    return(
+        <div className='post'>
+            <div className='post__content'>
+                <strong> {props.number}. {props.post.title}</strong>
+                <div>
+                    {props.post.body}
+                </div>
+                </div>
+            <div className='post__btns'>
+                    <Mybutton onClick={() => props.remove(props.post)}>Remove post</Mybutton>
+           </div>
+         </div>
+        
+    )
+}
+export default PostItem;
